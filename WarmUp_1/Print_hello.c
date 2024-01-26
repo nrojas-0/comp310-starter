91360edef4b470fd73307d2a456528d06f4979ec
Nancy Rojas <nrojas2@luc.edu>
2024-01-26 16:01:50 +0000

    adding WarmUp_1 hello world

---
+++ WarmUp_1/Print_hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS; //got this from the chapter 1 notes that were shown in class, so i decided to use this instead of 0
+}
