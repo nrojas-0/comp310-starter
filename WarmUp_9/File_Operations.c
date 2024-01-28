a955fbb2e7c1823bf29bb0367343031ab79f8c1c
Nancy Rojas <nrojas2@luc.edu>
2024-01-28 23:40:25 +0000

    adding WarmUp_9

---
+++ WarmUp_9/File_Operations.c
@@ -0,0 +1,28 @@
+//Objective: Write a program to create a file, write some data into it, and then read the data from the file.
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    char filename = "example.txt";
+    //Used https://www.geeksforgeeks.org/basics-file-handling-c/ as reference because I didn't know the commands for fileoperations in c
+    //Write in file
+    FILE *fw = fopen(filename, "w"); //Open for writing in text mode
+    fprintf(fw, "Hello, World!");
+    fclose(fw);
+
+    return 0;
+}
+/*
+import java.io.*;
+
+public class FileOperations {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+        br.close();
+    }
+}
+*/
