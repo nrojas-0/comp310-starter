46f20c8df5d478b940627fbf96f9d58538adad36
Nancy Rojas <nrojas2@luc.edu>
2024-01-29 01:31:03 +0000

    Added main section to file

--- WarmUp_8/LinkedList.c
+++ WarmUp_8/LinkedList.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+//Used Chatgpt to understand how to implement the linked list in C without the use of multiple other libraries
+
 int main(){
+    struct LinkedList llist;
+    llist.push(7); //Add numbers to linked list
+    llist.push(1);
+    llist.push(3);
+    llist.push(2);
+    printf("\nCreated Linked list is: \n");
+    printList(&llist);
+
+    deleteNode(&llist, 1); //deleting from linked list
+
+    printf("\nLinked List after Deletion of 1: \n");
+    printList(&llist);
+    printf("\n");
     return 0;
 }
