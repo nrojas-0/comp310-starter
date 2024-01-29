4fea2201a4763a64d8c6c4705c08a77553a0a581
Nancy Rojas <nrojas2@luc.edu>
2024-01-29 00:08:30 +0000

    adding exercise 9

--- WarmUp_9/File_Operations.c
+++ WarmUp_9/File_Operations.c
@@ -9,5 +9,16 @@ int main(){
     fprintf(fw, "Hello, World!");
     fclose(fw);
     //Read from file
+    FILE *fr = fopen(filename, "r"); // Open for reading in text mode
+    if (fr == NULL) {
+        perror("Error opening file for reading");
+        return 1;
+    }
+
+    char buffer[100]; //I don't really know why we need this?
+    fscanf(fr, "%99[^\n]", buffer); // Read up to 99 characters until a newline is encountered
+    printf("Data read from file: %s\n", buffer);
+
+    fclose(fr);
     return 0;
 }
