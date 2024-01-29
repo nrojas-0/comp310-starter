6fd21ebcac4af1f3658edd0058e14eec30659a57
Nancy Rojas <nrojas2@luc.edu>
2024-01-29 01:02:02 +0000

    Adding WarmUp_7 to repo

--- WarmUp_7/Linear_Search.c
+++ WarmUp_7/Linear_Search.c
@@ -1,4 +1,28 @@
+//Objective: Create a program to perform a linear search on an array of integers.
+/*
+cmake -DCMAKE_BUILD_TYPE=Debug -S . -B build
+      cmake --build build
+*/
 #include <stdio.h>
+#include <stdlib.h>
+int linearSearch(int arr[], int target){
+    int length = sizeof(arr);
+    for (int i = 0; i < length; i++){
+        if (arr[i] == target) {
+                return i;
+            }
+    }
+    return -1;
+
+}
 int main(){
+    int arr[] = {1, 4, 7, 9, 12};
+    int target = 9;
+    int index = linearSearch(arr, target);
+    if(index != -1){
+        printf("Element found at index: %d\n", index);
+    } else{
+        printf("Element not found\n");
+    }
     return 0;
 }
