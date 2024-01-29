ee380ebe82827c0c8c5d49592bc81790387812cd
Nancy Rojas <nrojas2@luc.edu>
2024-01-29 00:44:54 +0000

    Adding WarmUp_6 to repo

--- WarmUp_6/Factorial_calculation.c
+++ WarmUp_6/Factorial_calculation.c
@@ -1,4 +1,19 @@
+/*
+Objective: Implement a program to calculate the factorial of a number using recursion.
+Please note that I do not recommend recursion for this particular task but want to make sure you know how to do it at a basic level.
+(The escape is also guarded properly now as well.)
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
+int Factorial(int number){
+    if(number == 0){
+        return 1;
+    }
+    return number * Factorial(number-1);
+}
 int main(){
+    int number = 5;
+    printf("Factorial of %d is: %d\n", number, Factorial(number));
     return 0;
 }
